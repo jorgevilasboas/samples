aeb735938df05a85907ec833b076412ea399f115
jorgevilasboas <jorge.sergipe@gmail.com>
2019-09-20 16:17:39 -0300

    uploading some react native samples

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './screens/HomeScreen';
+import ComponentsScreen from './screens/ComponentsScreen';
+import ListScreen from './screens/ListScreen';
+import ImageScreen from './screens/ImageScreen';
+import CounterScreen from './screens/CounterScreen';
+import ColorScreen from './screens/ColorScreen';
+import SquareScreen from './screens/SquareScreen';
+import TextScreen from './screens/TextScreen';
+import BoxScreen from './screens/BoxScreen';
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen,
+    Color: ColorScreen,
+    Square: SquareScreen,
+    Text: TextScreen,
+    Box: BoxScreen
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
